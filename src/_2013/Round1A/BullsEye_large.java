d2d38572c0affba621f01d70827e4915e6ec3733
Wilson Camara Marriel <wilsonmarriel@gmail.com>
2014-04-24 21:30:53 -0300

    Added BullsEye Large

---
+++ src/_2013/Round1A/BullsEye_large.java
@@ -0,0 +1,123 @@
+package _2013.Round1A;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.math.BigInteger;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
+import java.util.Locale;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class BullsEye_large {
+	final static int DEBUG_LEVEL = 5;
+	final static int TIMER_LEVEL = 5;
+
+	final static String FILE_NAME = "A-large-practice";// <<<--------
+
+	final static String BASE = "C:/CodeJam/";
+	final static String SOURCE_FOLDER = "src";// <--- Eclipse standard
+	final static String INPUT = BASE + FILE_NAME + ".in";
+	final static String OUTPUT = BASE + FILE_NAME + ".out";
+	final static String FILE_SEPARATOR = System.getProperty("file.separator");
+	static Scanner in;
+	static PrintWriter out;
+	static long startTime;
+	static BigInteger r = BigInteger.ZERO;
+
+	private static void caseSolver() {
+		r = in.nextBigInteger();
+		BigInteger t = in.nextBigInteger();
+		long esq = 1L;
+		long dir = 1L << 62;
+
+		while (dir - esq > 1) {
+			long mid = (dir + esq) / 2;
+			if (f(mid).compareTo(t) > 0)
+				dir = mid;
+			else
+				esq = mid;
+		}
+		out.print(esq);
+	}
+
+	static BigInteger f(long x) {
+		BigInteger y = BigInteger.valueOf(x);
+		return r.add(y).multiply(BigInteger.valueOf(2)).subtract(BigInteger.ONE).multiply(y);
+	}
+
+	// XXXXXXXXXXXXXXXXXX-----CODEJAM TEMPLATE-----XXXXXXXXXXXXXXXXXX
+	public static void main(String[] args) {
+		try {
+			in = new Scanner(new FileReader(INPUT));
+			in.useLocale(Locale.US);
+			out = new PrintWriter(OUTPUT);
+
+			int number_of_cases = in.nextInt();
+			in.nextLine();
+			for (int c = 0; c < number_of_cases; c++) {
+				out.print("Case #" + (c + 1) + ": ");
+				log("Solving case " + (c + 1) + "...", 1);
+				startTimer(1);
+				caseSolver();
+				endTimer(1);
+				out.println();
+			}
+			out.println();
+			out.flush();
+			out.close();
+			in.close();
+			copySourceCode();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private static void copySourceCode() {
+		String classPath = System.getProperty("user.dir") + FILE_SEPARATOR + SOURCE_FOLDER;
+
+		String className = new Object() {
+		}.getClass().getEnclosingClass().getCanonicalName();
+
+		StringTokenizer st = new StringTokenizer(className, ".");
+
+		while (st.hasMoreElements()) {
+			String elementName = st.nextElement().toString();
+			classPath += FILE_SEPARATOR + elementName;
+			if (!st.hasMoreElements())
+				className = elementName;
+		}
+
+		classPath += ".java";
+
+		try {
+			Files.copy(new File(classPath).toPath(), new File(BASE + className + ".java").toPath(),
+					StandardCopyOption.REPLACE_EXISTING);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private static void log(String message, int d_level) {
+		if (DEBUG_LEVEL >= d_level) {
+			System.out.println(message);
+		}
+	}
+
+	private static void startTimer(int t_level) {
+		if (TIMER_LEVEL >= t_level) {
+			startTime = System.currentTimeMillis();
+		}
+	}
+
+	private static void endTimer(int t_level) {
+		if (TIMER_LEVEL >= t_level) {
+			long endTime = System.currentTimeMillis();
+			System.out.println((endTime - startTime) + " ms");
+		}
+	}
+	// XXXXXXXXXXXXXXXXXX-----CODEJAM TEMPLATE-----XXXXXXXXXXXXXXXXXX
+}
---
+++ src/_2013/Round1A/BullsEye_small.java
@@ -0,0 +1,63 @@
+package _2013.Round1A;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+public class BullsEye_small {
+	final static String base = "C:/CodeJam/A-large-practice";
+	final static String input = base + ".in";
+	final static String output = base + ".out";
+	static Scanner in;
+	static PrintWriter out;
+
+	public static void main(String[] args) {
+		try {
+			in = new Scanner(new FileReader(input));
+			out = new PrintWriter(output);
+
+			int number_of_cases = in.nextInt();
+			in.nextLine();
+			for (int c = 0; c < number_of_cases; c++) {
+				System.out.println("Testing case " + (c + 1) + "...");
+				out.print("Case #" + (c + 1) + ": ");
+				caseSolver();
+				out.println();
+			}
+			out.println();
+			out.flush();
+			out.close();
+			in.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	private static Long lowSQRT(Long num){
+		return Math.round(StrictMath.sqrt(num.doubleValue())) - 1;
+	}
+
+	private static void caseSolver() {
+		long startTime = System.nanoTime();
+
+		Long r = in.nextLong();
+		Long t = in.nextLong();
+		Long x = new Long(1);
+		
+		x = lowSQRT(t)/ 2  - (long)(0.148 * (double)r);
+		
+		System.out.println(x.toString());
+		
+		if(x < 0) x = new Long(1);
+		
+		while(2*r*x-x+2*x*x <= t)
+			x++;
+		
+		out.print(x-1);
+		
+		long endTime = System.nanoTime();
+		Double sec = ((double)(endTime - startTime) / 1000000000.0);
+		System.out.println(sec.toString());
+	}
+}
